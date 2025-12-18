74e358cc792d1e468448f66deb5fad3b27174d3c
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-17 18:30:18 -0800

    agent: Implement basic docker pull

--- agent/src/main.rs
+++ agent/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use bollard::{Docker, query_parameters::CreateImageOptionsBuilder};
+use tokio_stream::StreamExt;
+
+#[tokio::main]
+async fn main() {
+    let docker = bollard::Docker::connect_with_defaults().unwrap();
+    pull_image(&docker, "alpine", "3").await;
+}
+
+async fn pull_image(docker: &Docker, name: &str, tag: &str) {
+    let options = CreateImageOptionsBuilder::new()
+        .from_image(name)
+        .tag(tag)
+        .build();
+
+    let mut create_image_stream = docker.create_image(Some(options), None, None);
+
+    while let Some(info) = create_image_stream.next().await {
+        match info {
+            Ok(info) => {
+                println!("{}: {}", info.id.unwrap(), info.status.unwrap());
+            }
+            Err(e) => {
+                eprintln!("Error: {}", e);
+            }
+        }
+    }
 }
