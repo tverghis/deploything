138c8fb71c751a49220b049a1e1576b32076976a
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-19 16:21:07 -0800

    Add tracing and tracing-subscriber

--- agent/src/docker_api/image.rs
+++ agent/src/docker_api/image.rs
@@ -1,9 +1,12 @@
 use bollard::{Docker, query_parameters::CreateImageOptionsBuilder};
 use tokio_stream::StreamExt;
+use tracing::{error, info, instrument};
 
 use crate::docker_api::errors::DockerApiError;
 
+#[instrument(skip(docker))]
 pub async fn pull(docker: &Docker, name: &str, tag: &str) -> Result<(), DockerApiError> {
+    info!("Pulling image");
     let options = CreateImageOptionsBuilder::new()
         .from_image(name)
         .tag(tag)
@@ -14,12 +17,14 @@ pub async fn pull(docker: &Docker, name: &str, tag: &str) -> Result<(), DockerAp
     let infos: Result<Vec<_>, _> = create_image_stream.collect().await;
 
     if let Err(e) = infos {
-        eprintln!("error: {e}");
+        error!("Pull failed: {e}");
         return Err(DockerApiError::ImagePulledFailed {
             image: name.to_string(),
             tag: tag.to_string(),
         });
     }
+
+    info!("Pull complete");
 
     Ok(())
 }
--- agent/src/main.rs
+++ agent/src/main.rs
@@ -2,6 +2,9 @@ use agent::docker_api;
 
 #[tokio::main]
 async fn main() {
+    let subscriber = tracing_subscriber::FmtSubscriber::new();
+    tracing::subscriber::set_global_default(subscriber).unwrap();
+
     let docker = bollard::Docker::connect_with_defaults().unwrap();
     docker_api::image::pull(&docker, "alpine", "3")
         .await
