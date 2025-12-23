aa719dd054f97caf97d5325ae8e3ed29290bab6c
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-22 22:31:05 -0800

    Spawn container on receiving command

--- agent/agent_bin/src/cmd/handler.rs
+++ agent/agent_bin/src/cmd/handler.rs
@@ -1,18 +1,35 @@
 use agent_wire::deploything::v1::RunApplication;
+use bollard::Docker;
 use tokio::sync::mpsc::Receiver;
+use tracing::{info, instrument};
+
+use crate::docker_api::{Container, ContainerHostConfig};
 
 pub struct CommandHandler {
     cmd_rx: Receiver<RunApplication>,
+    docker: Docker,
 }
 
 impl CommandHandler {
     pub fn new(cmd_rx: Receiver<RunApplication>) -> Self {
-        Self { cmd_rx }
+        let docker = Docker::connect_with_defaults().unwrap();
+        Self { cmd_rx, docker }
     }
 
+    #[instrument(skip(self))]
     pub async fn handle_incoming(&mut self) {
         while let Some(cmd) = self.cmd_rx.recv().await {
-            dbg!(cmd);
+            info!("Received a RunApplication command from the server");
+            let host_config = ContainerHostConfig::With {
+                port_map: ("8080/tcp", "8080").into(),
+            };
+            let _ = Container::spawn_from_image(
+                &self.docker,
+                &cmd.image_name(),
+                &cmd.tag(),
+                host_config,
+            )
+            .await;
         }
     }
 }
