d33e6845f22416a5979c57c555679a6272eec6b1
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-22 22:04:58 -0800

    Open websocket connection

--- agent/agent_bin/src/main.rs
+++ agent/agent_bin/src/main.rs
@@ -1,21 +1,32 @@
-use agent_bin::docker_api::{Container, ContainerHostConfig};
+use tokio_stream::StreamExt;
+use tokio_tungstenite::tungstenite::Message;
+use tracing::{error, info};
 
 #[tokio::main]
 async fn main() {
     let subscriber = tracing_subscriber::FmtSubscriber::new();
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
-    let docker = bollard::Docker::connect_with_defaults().unwrap();
-    let container = Container::spawn_from_image(
-        &docker,
-        "mccutchen/go-httpbin",
-        "latest",
-        ContainerHostConfig::With {
-            port_map: ("8080/tcp", "8080").into(),
-        },
-    )
-    .await
-    .unwrap();
+    let request = "ws://localhost:4040";
+    let (mut stream, _) = tokio_tungstenite::connect_async(request).await.unwrap();
 
-    container.stop().await.unwrap();
+    while let Some(message) = stream.next().await {
+        match message {
+            Ok(message) => match message {
+                Message::Text(bytes) => {
+                    let text = bytes.as_str();
+                    info!("Got message: {text:?} from the server!");
+                }
+                Message::Close(_) => {
+                    info!("Server closed the connection cleanly.");
+                    break;
+                }
+                _ => unreachable!(),
+            },
+            Err(e) => {
+                error!("Unexpected error in websocket stream: {e}");
+                break;
+            }
+        }
+    }
 }
