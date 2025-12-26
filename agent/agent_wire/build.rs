1a4fca38f1a6a11abb1bca0c63c0d33a264fc04e
Tarun Verghis <tarun.verghis@gmail.com>
2025-12-25 16:06:39 -0800

    Add initial definition of AgentSnapshot

--- agent/agent_wire/build.rs
+++ agent/agent_wire/build.rs
@@ -1,6 +1,9 @@
 fn main() {
     prost_build::compile_protos(
-        &["../../protos/deploything/v1/remote_command.proto"],
+        &[
+            "../../protos/deploything/v1/remote_command.proto",
+            "../../protos/deploything/v1/agent_snapshot.proto",
+        ],
         &["../../protos"],
     )
     .unwrap();
